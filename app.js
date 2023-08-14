d8a0a42db70ef470bcd231a3d8c67e61e48bcab6
Emre Turan <itsemreturan@gmail.com>
2023-08-14 22:15:18 +0300

    fixed issue

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
+document.querySelector('form').addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  // Collect form data
+  const formData = {
+    name: document.querySelector('input[name="name"]').value,
+    email: document.querySelector('input[name="email"]').value,
+    phone: document.querySelector('input[name="phone"]').value,
+    message: document.querySelector('textarea[name="message"]').value,
+  };
+
+  // Send email (you'll need to integrate with an email service like EmailJS)
+  // Example:
+  // emailjs.send('service_id', 'template_id', formData, 'user_id');
+
+  // Display the message below the 'Submit' button
+  document.getElementById('submit-message').style.display = 'block';
+  document.getElementById('submit-message').innerHTML =
+    'Your form is submitted. Please <a href="final-step.html" style="color: #0f0f0f;">click here</a> to proceed to the final step of registration.';
+});
+
+// Existing code
 const inputs = document.querySelectorAll(".input");
 
 function focusFunc() {
